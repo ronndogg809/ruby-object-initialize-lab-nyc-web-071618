c550c1c83621aa4d90554b621b440cb786ad2d15
github username <github email address>
2018-07-18 14:46:21 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize(name)
+      @name = name
+    end
+
+    def name
+      @name
+    end
+
+    def name=(new_name)
+      @name = new_name
+    end
+
+end
+
+
+
+
+# this will set the name of a person in the instnace variable @name
+# now we can call on it edit it or figure out what it is 
